e1f58b2e88e1dc26d15a7738598529d7037755e9
waonpad <waonpad@gmail.com>
2023-10-15 03:14:51 +0900

    feat(なし): ✨ 不要になった記述を削除

--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -1,19 +1,3 @@
-import type { FieldValues, RegisterOptions, Path } from 'react-hook-form';
-
-// Test
-export type User = { email: string };
-
-export type BaseEntity = {
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
-export type ReactHookFormValidationRules<T extends FieldValues> = Record<
-  keyof T,
-  Omit<RegisterOptions<T, Path<T>>, 'value'> | undefined
->;
-
 export type ErrorResponse = {
   statusCode: number;
   message: string;
