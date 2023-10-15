286edb0f974829e7c8e4cc737276a798015b3b94
waonpad <waonpad@gmail.com>
2023-10-16 03:03:17 +0900

    config(設定ファイル): ⚙ shadcn/ui導入のための設定

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ['custom/react-internal'],
+  extends: ['custom/react'],
+  settings: {
+    tailwindcss: {
+      config: './tailwind.config.js',
+    },
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  rules: {
+    'react-refresh/only-export-components': 'off',
+  },
 };
