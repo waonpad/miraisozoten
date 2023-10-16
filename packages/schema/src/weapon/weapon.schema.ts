8fc8bc71e4af77abb04395980cc4249d10cbe2e3
waonpad <waonpad@gmail.com>
2023-10-16 10:59:47 +0900

    fix(なし): 🐞 enumを直接importするとエラーが出るのでDBと同じ定義をハードコードしている

--- packages/schema/src/weapon/weapon.schema.ts
+++ packages/schema/src/weapon/weapon.schema.ts
@@ -1,21 +1,12 @@
+import { type Prisma, type Weapon } from 'database';
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'nestjs-zod/z';
 
-import type { Prisma, Weapon } from 'database';
-
-export enum WeaponAttribute {
-  Sword = 'sword',
-  Bow = 'bow',
-  Spear = 'spear',
-  Axe = 'axe',
-  Dagger = 'dagger',
-}
-
 const WeaponShema = z.object({
-  id: z.number(),
+  id: z.coerce.number(),
   name: z.string(),
-  attackPower: z.number(),
-  attribute: z.nativeEnum(WeaponAttribute),
+  attackPower: z.coerce.number(),
+  attribute: z.enum(['SWORD', 'BOW']),
 });
 
 // id は autoincrement で生成されるので除外する
