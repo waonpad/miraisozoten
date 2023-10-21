7c01f34a93c9c1fe5019b3689ae853a6204fe5d7
waonpad <waonpad@gmail.com>
2023-10-21 14:43:48 +0900

    test(api): 🧪 認証ありのテストをdreddでできるように修正

--- apps/api/test/dredd/create-weapon.hook.js
+++ apps/api/test/dredd/create-weapon.hook.js
@@ -1,11 +1,33 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const hooks = require('hooks');
+const axios = require('axios');
+
+require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env.test.local' });
 
 hooks.before('/weapons > POST > 201', async (transaction, done) => {
-  transaction.skip = true; // テストをスキップする
+  const newWeapon = {
+    name: 'new weapon',
+    attackPower: 100,
+    attribute: 'SWORD',
+  };
 
-  hooks.log('テストをスキップしました。delete, updateもスキップされています。');
-  hooks.log('ユーザー認証をモックする機能が未実装です。');
+  transaction.request.body = JSON.stringify(newWeapon);
+
+  done();
+});
+
+hooks.after('/weapons > POST > 201', async (transaction, done) => {
+  const deleteRequestPath = `/weapons/${JSON.parse(transaction.real.body).id}`;
+
+  const res = await axios.delete(
+    `http://${process.env.HOST}:${process.env.PORT}` + deleteRequestPath,
+    {
+      headers: {
+        Authorization: 'Bearer ' + process.env.VALID_TOKEN,
+      },
+    }
+  );
 
   done();
 });
--- apps/api/test/dredd/delete-weapon.hook.js
+++ apps/api/test/dredd/delete-weapon.hook.js
@@ -3,16 +3,25 @@ const hooks = require('hooks');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const axios = require('axios');
 
-hooks.before('/weapons/{id} > DELETE > 204', async (transaction, done) => {
+require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env.test.local' });
+
+hooks.before('/weapons/{id} > DELETE > 200', async (transaction, done) => {
   const newWeapon = {
     name: 'new weapon',
     attackPower: 100,
     attribute: 'SWORD',
   };
 
-  const res = await axios.post(`http://${process.env.HOST}:${process.env.PORT}/weapons`, newWeapon);
-
-  hooks.log('削除されるためのデータを作成しました id: ' + res.data.id);
+  const res = await axios.post(
+    `http://${process.env.HOST}:${process.env.PORT}/weapons`,
+    newWeapon,
+    {
+      headers: {
+        Authorization: 'Bearer ' + process.env.VALID_TOKEN,
+      },
+    }
+  );
 
   const deleteRequestPath = `/weapons/${res.data.id}`;
 
