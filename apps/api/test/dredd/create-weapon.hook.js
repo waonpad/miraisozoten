23142b8a154c7bf992bc9fc6f08bfed18a0ae003
waonpad <waonpad@gmail.com>
2023-10-21 15:24:51 +0900

    fix(api): 🐞 テストパスの記述ミスを修正

--- apps/api/test/dredd/create-weapon.hook.js
+++ apps/api/test/dredd/create-weapon.hook.js
@@ -5,29 +5,35 @@ const axios = require('axios');
 require('dotenv').config({ path: '.env' });
 require('dotenv').config({ path: '.env.test.local' });
 
-hooks.before('/weapons > POST > 201', async (transaction, done) => {
-  const newWeapon = {
-    name: 'new weapon',
-    attackPower: 100,
-    attribute: 'SWORD',
-  };
+hooks.before(
+  '/weapons > POST > 201 > application/json; charset=utf-8',
+  async (transaction, done) => {
+    const newWeapon = {
+      name: 'new weapon',
+      attackPower: 100,
+      attribute: 'SWORD',
+    };
 
-  transaction.request.body = JSON.stringify(newWeapon);
+    transaction.request.body = JSON.stringify(newWeapon);
 
-  done();
-});
+    done();
+  }
+);
 
-hooks.after('/weapons > POST > 201', async (transaction, done) => {
-  const deleteRequestPath = `/weapons/${JSON.parse(transaction.real.body).id}`;
+hooks.after(
+  '/weapons > POST > 201 > application/json; charset=utf-8',
+  async (transaction, done) => {
+    const deleteRequestPath = `/weapons/${JSON.parse(transaction.real.body).id}`;
 
-  const res = await axios.delete(
-    `http://${process.env.HOST}:${process.env.PORT}` + deleteRequestPath,
-    {
-      headers: {
-        Authorization: 'Bearer ' + process.env.VALID_TOKEN,
-      },
-    }
-  );
+    const res = await axios.delete(
+      `http://${process.env.HOST}:${process.env.PORT}` + deleteRequestPath,
+      {
+        headers: {
+          Authorization: 'Bearer ' + process.env.VALID_TOKEN,
+        },
+      }
+    );
 
-  done();
-});
+    done();
+  }
+);
--- apps/api/test/dredd/delete-weapon.hook.js
+++ apps/api/test/dredd/delete-weapon.hook.js
@@ -6,27 +6,30 @@ const axios = require('axios');
 require('dotenv').config({ path: '.env' });
 require('dotenv').config({ path: '.env.test.local' });
 
-hooks.before('/weapons/{id} > DELETE > 200', async (transaction, done) => {
-  const newWeapon = {
-    name: 'new weapon',
-    attackPower: 100,
-    attribute: 'SWORD',
-  };
+hooks.before(
+  '/weapons/{id} > DELETE > 200 > application/json; charset=utf-8',
+  async (transaction, done) => {
+    const newWeapon = {
+      name: 'new weapon',
+      attackPower: 100,
+      attribute: 'SWORD',
+    };
 
-  const res = await axios.post(
-    `http://${process.env.HOST}:${process.env.PORT}/weapons`,
-    newWeapon,
-    {
-      headers: {
-        Authorization: 'Bearer ' + process.env.VALID_TOKEN,
-      },
-    }
-  );
+    const res = await axios.post(
+      `http://${process.env.HOST}:${process.env.PORT}/weapons`,
+      newWeapon,
+      {
+        headers: {
+          Authorization: 'Bearer ' + process.env.VALID_TOKEN,
+        },
+      }
+    );
 
-  const deleteRequestPath = `/weapons/${res.data.id}`;
+    const deleteRequestPath = `/weapons/${res.data.id}`;
 
-  transaction.request.uri = deleteRequestPath;
-  transaction.fullPath = deleteRequestPath;
+    transaction.request.uri = deleteRequestPath;
+    transaction.fullPath = deleteRequestPath;
 
-  done();
-});
+    done();
+  }
+);
