1378bbfb1b7c695c27240d4fa900ba0692efb1de
waonpad <waonpad@gmail.com>
2023-11-03 02:11:46 +0900

    refactor(none): 🔨 変更の反映

--- apps/web/src/constants/cookie-names.ts
+++ apps/web/src/constants/cookie-names.ts
@@ -1,4 +1,4 @@
 export const COOKIE_NAMES = {
-  AUTH_TOKEN: 'react-oauth-token',
+  // AUTH_TOKEN: 'react-oauth-token', // firebaseの認証を使うようにしたのでcookieが不要になった
   CURRENT_TODOFUKEN_GAME_ID: 'todofuken-game-id',
 };
