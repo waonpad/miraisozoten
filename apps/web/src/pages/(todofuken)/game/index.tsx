e52d3c360ae7ec79992c2da41c47bf61e8baab45
waonpad <waonpad@gmail.com>
2023-11-05 16:10:33 +0900

    fix(web): 🐞 screenがnullの可能性があるため、コードに反映

--- apps/web/src/pages/(todofuken)/game/index.tsx
+++ apps/web/src/pages/(todofuken)/game/index.tsx
@@ -4,5 +4,6 @@ import { useGame } from '../_/hooks/use-game';
 export default function Page() {
   const { screen } = useGame();
 
-  return GameScreen[screen];
+  // screenがnullの場合はローディング画面を表示しているので、確実にscreenはnullではない
+  return GameScreen[screen!];
 }
