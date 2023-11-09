a0beaf677fdab0898861a60a76d1582e613a6561
waonpad <waonpad@gmail.com>
2023-11-09 15:32:44 +0900

    feat(web): ✨ マップで沖縄を折り返したときにわかりやすくするための線を作成した

--- apps/web/src/components/maps/japan-radio-svg-map.tsx
+++ apps/web/src/components/maps/japan-radio-svg-map.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 import Japan from '@svg-maps/japan';
 import { Prefecture } from 'database';
 import { SVGMap, SVGMapProps } from 'react-svg-map';
+
 import 'react-svg-map/lib/index.css';
 
 export type JapanRadioSVGMapProps = Omit<SVGMapProps, 'map'> & {
@@ -36,12 +38,21 @@ export const JapanRadioSVGMap = (props: JapanRadioSVGMapProps) => {
   }, [props.selected]);
 
   useEffect(() => {
+    // 要素の初期化
+    resetJapanSvg();
+
     if (props.relocation?.okinawa && !props.relocation?.hokkaido) {
       relocationOnlyOkinawa();
     } else if (!props.relocation?.okinawa && props.relocation?.hokkaido) {
       relocationOnlyHokkaido();
     } else if (props.relocation?.okinawa && props.relocation?.hokkaido) {
       relocationOkinawaAndHokkaido();
+    }
+
+    // NOTICE: 北海道の位置を変えることは想定していない
+    // NOTICE: 北海道の位置も変える場合はまた調整する
+    if (props.relocation?.okinawa && !props.relocation?.hokkaido) {
+      addOkinwaSeparator();
     }
   }, [props.relocation]);
 
@@ -54,12 +65,44 @@ export const JapanRadioSVGMap = (props: JapanRadioSVGMapProps) => {
   );
 };
 
+const japanSvgClassName = 'svg-map';
+const japanSvgDefaultViewBox = '0 0 438 516';
+const getJapanSvg = () => document.getElementsByClassName(japanSvgClassName)[0];
+
+const prefectureSvgClassName = 'svg-map__location';
+const separatorClassName = 'prefcture-separator';
+const okinawaSeparatorId = 'okinawa-separator';
+
+/**
+ * JSで操作した要素をまとめてリセットする関数
+ */
+const resetJapanSvg = () => {
+  const japanSvg = getJapanSvg();
+
+  // viewboxをデフォルトに戻す
+  japanSvg?.setAttribute('viewBox', japanSvgDefaultViewBox);
+
+  const prefectures = document.getElementsByClassName(prefectureSvgClassName);
+
+  // 都道府県の移動をリセット
+  Array.from(prefectures).forEach((prefecture) => {
+    prefecture.setAttribute('transform', '');
+  });
+
+  const separators = document.getElementsByClassName(separatorClassName);
+
+  // セパレーターを削除
+  Array.from(separators).forEach((separator) => {
+    separator.remove();
+  });
+};
+
 /**
  * 沖縄県だけ位置を変更する
  */
 const relocationOnlyOkinawa = () => {
   // svgタグを取得
-  const japanSvg = document.getElementsByClassName('svg-map')[0];
+  const japanSvg = getJapanSvg();
 
   // viewboxを変更
   japanSvg?.setAttribute('viewBox', '0 0 438 385');
@@ -75,7 +118,7 @@ const relocationOnlyOkinawa = () => {
  */
 const relocationOnlyHokkaido = () => {
   // svgタグを取得
-  const japanSvg = document.getElementsByClassName('svg-map')[0];
+  const japanSvg = getJapanSvg();
 
   // viewboxを変更
   japanSvg?.setAttribute('viewBox', '0 107 438 411');
@@ -91,7 +134,7 @@ const relocationOnlyHokkaido = () => {
  */
 const relocationOkinawaAndHokkaido = () => {
   // svgタグを取得
-  const japanSvg = document.getElementsByClassName('svg-map')[0];
+  const japanSvg = getJapanSvg();
 
   // viewboxを変更
   japanSvg?.setAttribute('viewBox', '0 107 438 277');
@@ -102,4 +145,32 @@ const relocationOkinawaAndHokkaido = () => {
   // 移動
   hokkaido?.setAttribute('transform', 'translate(-220, 120)');
   okinawa?.setAttribute('transform', 'translate(300, -135)');
+};
+
+/**
+ * 位置を移動させたことをわかりやすくするため、
+ * 沖縄県と本州の境界線を引く
+ */
+const addOkinwaSeparator = () => {
+  if (document.getElementById(okinawaSeparatorId)) return;
+
+  const okinawaSeparator = (
+    <path
+      id={okinawaSeparatorId}
+      className={separatorClassName}
+      d="M 290 360 L 380 285 Z"
+      stroke="gray"
+      strokeWidth="2"
+    />
+  );
+  const okinawaSeparatorString = ReactDOMServer.renderToString(okinawaSeparator);
+
+  const japanSvg = getJapanSvg();
+
+  // https://developer.mozilla.org/ja/docs/Web/API/Element/insertAdjacentHTML
+  japanSvg?.insertAdjacentHTML('beforeend', okinawaSeparatorString);
+
+  // 通常のjsのように要素を作成する場合、createElemetNSを使う
+  // (svgはcreateElemetでは追加できない)
+  // https://developer.mozilla.org/ja/docs/Web/API/Document/createElementNS
 };
