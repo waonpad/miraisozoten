9b3957ad7aaedfb0c689a0bf0387eef4784a656a
waonpad <waonpad@gmail.com>
2023-11-14 16:22:59 +0900

    feat(web): ✨ close #188

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -19,6 +19,7 @@ export default ({ mode }) => {
   return defineConfig({
     plugins: [
       react(),
+      htmlPlugin(process.env),
       yaml(),
       generouted({
         source: {
@@ -34,3 +35,20 @@ export default ({ mode }) => {
     },
   });
 };
+
+/**
+ * Replace env variables in index.html
+ * @see https://github.com/vitejs/vite/issues/3105#issuecomment-939703781
+ * @example `%VITE_MY_ENV%`
+ * @see https://vitejs.dev/guide/api-plugin.html#transformindexhtml
+ */
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: {
+      enforce: 'pre' as const,
+      transform: (html: string): string =>
+        html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+    },
+  };
+}
