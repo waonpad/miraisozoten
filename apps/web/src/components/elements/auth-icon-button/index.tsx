b900fe2658d2c66edda54c838be65a7a506dfcad
waonpad <waonpad@gmail.com>
2023-11-14 15:40:06 +0900

    feat(none): ✨ close #199

--- apps/web/src/components/elements/auth-icon-button/index.tsx
+++ apps/web/src/components/elements/auth-icon-button/index.tsx
@@ -4,11 +4,19 @@ import { useAuth } from '@/auth/use-auth';
 export const AuthIconButton = () => {
   const { user, login, logout } = useAuth();
 
+  const isLoggedIn = !!user;
+
+  const isAnonymous = user?.email?.endsWith('@example.com');
+
   return (
     <>
-      {user ? (
+      {isLoggedIn && !isAnonymous && (
+        // ログインしていて、かつ匿名ユーザーでなければログアウトできる
         <button onClick={() => logout()}>ログアウト</button>
-      ) : (
+      )}
+      {((isLoggedIn && isAnonymous) || !isLoggedIn) && (
+        // ログインされていていも、匿名ユーザーなら実際のアカウントでログインできる
+        // ログインされていなければログインできる(自動で匿名ログインされるため、起こらないはず)
         <button onClick={() => login('google')}>ログイン</button>
       )}
     </>
