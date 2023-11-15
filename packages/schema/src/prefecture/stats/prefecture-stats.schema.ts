aff5a78be74e6ab780b407d74ed0560caa8a2f0b
waonpad <waonpad@gmail.com>
2023-11-16 01:44:25 +0900

    docs(api): ✏ スキーマにopenapiのexampleを追加した

--- packages/schema/src/prefecture/stats/prefecture-stats.schema.ts
+++ packages/schema/src/prefecture/stats/prefecture-stats.schema.ts
@@ -1,14 +1,18 @@
+import { extendZodWithOpenApi } from '@anatine/zod-openapi';
 import { z } from 'nestjs-zod/z';
+import { z as zod } from 'zod';
 
 import { PrefectureStatsConfig } from './prefecture-stats.config';
 
 import type { PrefectureStats } from 'database';
 
+extendZodWithOpenApi(z as typeof zod);
+
 export const PrefectureStatsShema = z.object({
-  id: z.number(),
+  id: z.number().openapi({ example: 1 }),
   ...(Object.entries(PrefectureStatsConfig)
     .map(([, value]) => ({
-      [value.camel]: z.number(),
+      [value.camel]: z.number().openapi({ example: 100 }),
     }))
     .reduce((acc, cur) => {
       return { ...acc, ...cur };
--- packages/schema/src/user/user.schema.ts
+++ packages/schema/src/user/user.schema.ts
@@ -1,26 +1,21 @@
-import { createZodDto } from 'nestjs-zod';
+import { extendZodWithOpenApi } from '@anatine/zod-openapi';
 import { z } from 'nestjs-zod/z';
+import { z as zod } from 'zod';
 
-import type { Prisma, User } from 'database';
+import type { User } from 'database';
+
+extendZodWithOpenApi(z as typeof zod);
 
 export const UserShema = z.object({
-  id: z.string(),
-  name: z.string(),
-  email: z.string().nullable(),
-  emailVerified: z.boolean(),
-  image: z.string().nullable(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  id: z.string().openapi({ example: '00000000-0000-0000-0000-000000000000' }),
+  name: z.string().openapi({ example: 'ゲスト' }),
+  email: z.string().nullable().openapi({ example: 'example@example.com' }),
+  emailVerified: z.boolean().openapi({ example: false }),
+  image: z.string().nullable().openapi({ example: null }),
+  createdAt: z.date().openapi({ example: '2021-01-01T00:00:00.000Z' }),
+  updatedAt: z.date().openapi({ example: '2021-01-01T00:00:00.000Z' }),
 });
-
-export const CreateUserInputSchema = UserShema.omit({
-  id: true,
-}) satisfies z.ZodType<Prisma.UserCreateInput>;
 
 export const UserResponseSchema: z.ZodType<User> = UserShema;
 
-export type CreateUserInput = z.infer<typeof CreateUserInputSchema>;
-
 export type UserResponse = z.infer<typeof UserResponseSchema>;
-
-export class CreateUserInputDto extends createZodDto(CreateUserInputSchema) {}
