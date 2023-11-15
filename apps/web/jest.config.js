81b74ab9b38d9f577f94cfec143a7460ba546926
waonpad <waonpad@gmail.com>
2023-11-16 02:53:25 +0900

    config(web): ⚙ デフォルトのテスト環境をjsdomに変更

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   collectCoverageFrom: ['src/**/*.{ts,tsx}', 'src/**/*.test.{ts,tsx}', '!src/**/*.d.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
