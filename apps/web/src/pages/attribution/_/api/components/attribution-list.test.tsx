aec0c8a52454ccff608fe6143dc6c8ae4ad9da21
waonpad <waonpad@gmail.com>
2023-11-18 17:01:12 +0900

    test(none): 🧪 お試しのテストファイルを作成した

---
+++ apps/web/src/pages/attribution/_/api/components/attribution-list.test.tsx
@@ -0,0 +1,15 @@
+import { AttributionList } from '@/pages/attribution/_/api/components/attribution-list';
+import { render, screen } from '@/test/utils';
+
+test('renders AttributionList', async () => {
+  await render(<AttributionList />);
+
+  const listItems = await screen.findAllByRole('listitem');
+
+  listItems.forEach((item) => {
+    expect(item).toBeInTheDocument();
+  });
+
+  // レンダリングされたhtmlを確認する場合
+  // screen.debug();
+});
