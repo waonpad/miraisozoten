9a7c71bc5cd26be685df7625dc563de6784a0d6c
waonpad <waonpad@gmail.com>
2023-11-21 21:02:56 +0900

    style(web): 👙 close #244

--- apps/web/src/components/layout/main/index.tsx
+++ apps/web/src/components/layout/main/index.tsx
@@ -1,16 +1,37 @@
 import * as React from 'react';
+import { useLocation } from 'react-router-dom';
 
-import { AppBar } from '../app-bar';
+import InkPaintingBg from '@/assets/ink-painting-bg.jpg';
 
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
+  const location = useLocation();
+
+  const isTopPage = location.pathname === '/';
+
   return (
     <>
-      <AppBar />
-      <main>{children}</main>
+      <main
+        style={{
+          ...(!isTopPage && {
+            backgroundImage: `url(${InkPaintingBg})`,
+            // 画像のopacityを擬似的に0.3にする
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            backgroundBlendMode: 'lighten',
+            // 背景の調整
+            backgroundPosition: 'left bottom',
+            backgroundSize: 'auto 112%',
+            backgroundRepeat: 'no-repeat',
+            backgroundAttachment: 'fixed',
+          }),
+          minHeight: '100vh',
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
