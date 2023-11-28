abaf390fadd7d898ddf1bf6b66fddec8c1ba2d35
waonpad <waonpad@gmail.com>
2023-11-28 15:07:22 +0900

    feat(web): ✨ close #270

--- apps/web/src/components/elements/auth-icon-button/index.tsx
+++ apps/web/src/components/elements/auth-icon-button/index.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 import { Button } from 'ui/components/ui/button';
 
 import LoginIcon from '@/assets/icons/login.svg?react';
 import LogoutIcon from '@/assets/icons/logout.svg?react';
 import { useAuth } from '@/auth/use-auth';
+
+import { ConfirmLogoutDialog } from '../confirm-logout-dialog';
 
 // TODO: アイコンボタンにして、propsも受け取れるようにする
 export const AuthIconButton = () => {
@@ -12,21 +16,41 @@ export const AuthIconButton = () => {
 
   const isAnonymous = user?.email?.endsWith('@example.com');
 
+  const [isConfirmLogoutDialogOpen, setIsConfirmLogoutDialogOpen] = useState(false);
+
+  const handleClickLogout = () => setIsConfirmLogoutDialogOpen(true);
+
+  const handleConfirmLogout = () => {
+    logout();
+
+    setIsConfirmLogoutDialogOpen(false);
+  };
+
+  const handleCloseConfirmLogoutDialog = () => setIsConfirmLogoutDialogOpen(false);
+
+  const handleClickLogin = () => login('google');
+
   return (
     <>
       {isLoggedIn && !isAnonymous && (
         // ログインしていて、かつ匿名ユーザーでなければログアウトできる
-        <Button onClick={logout} asChild size={'icon'} variant={'icon'}>
+        <Button onClick={handleClickLogout} asChild size={'icon'} variant={'icon'}>
           <LogoutIcon />
         </Button>
       )}
       {((isLoggedIn && isAnonymous) || !isLoggedIn) && (
         // ログインされていていも、匿名ユーザーなら実際のアカウントでログインできる
         // ログインされていなければログインできる(自動で匿名ログインされるため、起こらないはず)
-        <Button onClick={() => login('google')} asChild size={'icon'} variant={'icon'}>
+        <Button onClick={handleClickLogin} asChild size={'icon'} variant={'icon'}>
           <LoginIcon />
         </Button>
       )}
+
+      <ConfirmLogoutDialog
+        open={isConfirmLogoutDialogOpen}
+        handleConfirm={handleConfirmLogout}
+        handleOpenChange={handleCloseConfirmLogoutDialog}
+      />
     </>
   );
 };
