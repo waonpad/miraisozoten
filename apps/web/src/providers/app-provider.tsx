eaea0a66b2e885816f0fb256313c6445f223dd96
waonpad <waonpad@gmail.com>
2023-12-05 19:49:15 +0900

    perf(web): ⚡️ 不要になったファイルを削除

--- apps/web/src/providers/app-provider.tsx
+++ apps/web/src/providers/app-provider.tsx
@@ -11,7 +11,6 @@ import { SuspenseFallback } from '@/components/elements/suspense-fallback';
 import { WatchUnhandledError } from '@/lib/react-error-boundary';
 import { queryClient } from '@/lib/react-query';
 import * as Sentry from '@/lib/sentry';
-import { SoundEffectHandler } from '@/lib/use-sound/sound-effect-handler';
 import { SoundProvider } from '@/lib/use-sound/sound-provider';
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
@@ -21,17 +20,15 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
       <Sentry.ErrorBoundary fallback={ErrorFallback}>
         <WatchUnhandledError>
           <SoundProvider>
-            <SoundEffectHandler>
-              <Suspense fallback={<SuspenseFallback />}>
-                <HelmetProvider>
-                  <QueryClientProvider client={queryClient}>
-                    <AuthProvider>
-                      <AuthGuard>{children}</AuthGuard>
-                    </AuthProvider>
-                  </QueryClientProvider>
-                </HelmetProvider>
-              </Suspense>
-            </SoundEffectHandler>
+            <Suspense fallback={<SuspenseFallback />}>
+              <HelmetProvider>
+                <QueryClientProvider client={queryClient}>
+                  <AuthProvider>
+                    <AuthGuard>{children}</AuthGuard>
+                  </AuthProvider>
+                </QueryClientProvider>
+              </HelmetProvider>
+            </Suspense>
           </SoundProvider>
         </WatchUnhandledError>
       </Sentry.ErrorBoundary>
