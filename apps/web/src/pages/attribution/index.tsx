f0f3997d8a193ac39c55877b473ea3b415e2d24a
都築 <sarai.507@docomo.ne.jp>
2023-12-21 17:18:53 +0900

    style(design): 👙 デザインは完了

--- apps/web/src/pages/attribution/index.tsx
+++ apps/web/src/pages/attribution/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 
+import { Logo } from '@/components/elements/logo';
 import { Head } from '@/components/head';
 import { useFadeTransition } from '@/components/transitions/fade-transition/use-fade-transition';
 
 import { AttributionList } from './_/api/components/attribution-list';
+
+import './_/attribution.css'; // css読み込み
 
 export default function Page() {
   const fadeTransition = useFadeTransition();
@@ -22,8 +25,13 @@ export default function Page() {
         description="このサイトで現在使用しているデータの出典一覧です。"
       />
 
-      <h1>都道府県データの引用元</h1>
-      <AttributionList />
+      <div className="pagetitle">
+        <Logo />
+        <p>都道府県データの引用元</p>
+      </div>
+      <div className="attrtable">
+        <AttributionList />
+      </div>
     </>
   );
 }
