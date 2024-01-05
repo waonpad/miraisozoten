608798cdd4ffc25b1fdccb0e54426e6a157e3641
waonpad <waonpad@gmail.com>
2024-01-05 15:36:05 +0900

    style(design): 👙 close #284

--- apps/web/src/pages/(todoufuken)/_/components/game-turn-question.tsx
+++ apps/web/src/pages/(todoufuken)/_/components/game-turn-question.tsx
@@ -1,23 +1,7 @@
-import { useMemo } from 'react';
-
 import { HighLow } from 'schema/dist/todoufuken/game';
 
 export type GameTurnQuestionProps = {
   highLow: HighLow;
-};
-
-const MESSAGES = {
-  HIGH: ['HIGHテキスト1', 'HIGHテキスト2', 'HIGHテキスト3'],
-  LOW: ['LOWテキスト1', 'LOWテキスト2', 'LOWテキスト3'],
-} satisfies {
-  [key in HighLow]: [string, ...string[]];
-};
-
-const randomMessage = <T extends keyof typeof MESSAGES>(
-  hishLow: T
-): (typeof MESSAGES)[T][number] => {
-  // どちらかの配列からランダムに一つ取得する
-  return MESSAGES[hishLow][Math.floor(Math.random() * MESSAGES[hishLow].length)];
 };
 
 /**
@@ -25,7 +9,9 @@ const randomMessage = <T extends keyof typeof MESSAGES>(
  * 問題文を表示するコンポーネント
  */
 export const GameTurnQuestion = ({ highLow }: GameTurnQuestionProps) => {
-  const message = useMemo(() => randomMessage(highLow), [highLow]);
-
-  return <div className="text-xl sm:text-2xl lg:text-3xl">{message}</div>;
+  return (
+    <div className="text-xl sm:text-2xl lg:text-3xl">
+      {highLow === 'HIGH' ? '隣接する県より多いのはどれ？' : '隣接する県より少ないのはどれ？'}
+    </div>
+  );
 };
