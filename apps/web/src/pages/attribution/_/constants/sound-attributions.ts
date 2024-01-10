9bfc0eb4d2d9916fa3069c407bf88433b77a9693
waonpad <waonpad@gmail.com>
2024-01-10 15:45:36 +0900

    feat(web): ✨ close #343

---
+++ apps/web/src/pages/attribution/_/constants/sound-attributions.ts
@@ -0,0 +1,25 @@
+export const SoundAttributions: {
+  sourceSiteUrl: string;
+  sourceSiteName: string;
+}[] = [
+  {
+    sourceSiteUrl: 'https://on-jin.com/',
+    sourceSiteName: 'On-Jin ～音人～',
+  },
+  {
+    sourceSiteUrl: 'https://pocket-se.info/',
+    sourceSiteName: 'ポケットサウンド',
+  },
+  {
+    sourceSiteUrl: 'https://dova-s.jp/',
+    sourceSiteName: 'DOVA-SYNDROME',
+  },
+  {
+    sourceSiteUrl: 'http://oto-no-sono.com',
+    sourceSiteName: '音の園',
+  },
+  {
+    sourceSiteUrl: 'https://music.storyinvention.com/',
+    sourceSiteName: 'MOMIZizm MUSiC',
+  },
+];
