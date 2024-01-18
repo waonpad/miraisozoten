a20dfe3561000c98403cd8652f16a8f8c97b0d64
waonpad <waonpad@gmail.com>
2024-01-18 17:12:31 +0900

    fix(design): 🐞 close #363

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -16,6 +16,15 @@ module.exports = {
       },
     },
     extend: {
+      height: {
+        screen: ['100vh', '100dvh'],
+      },
+      minHeight: {
+        screen: ['100vh', '100dvh'],
+      },
+      maxHeight: {
+        screen: ['100vh', '100dvh'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
